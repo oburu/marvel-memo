52169c91a1033b35ac8fea044b5ca65ea8b02da0
oburu <oburusolo@gmail.com>
2017-04-25 11:21:58 +0100

    axios added, and comics of arrays created too

--- src/components/game_page.js
+++ src/components/game_page.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+import CryptoJS from 'crypto-js';
 import Card from './card';
 import Modal from './modal';
 
@@ -7,22 +9,58 @@ const COMICS = [
   {name:'wolverine'},{name:'spiderman'},{name:'daredevil'},{name:'x-men'},
   {name:'wolverine'},{name:'spiderman'},{name:'daredevil'},{name:'x-men'}
 ];
+const PRIV_KEY = "c489aff329d83d09815b554f38d843ba42a5061a";
+const PUBLIC_KEY = "2a7fca050595ffa66aaf74e2b1bae70f";
+const URL = 'https://gateway.marvel.com/v1/public/comics';
 
 class GamePage extends Component {
   constructor(props){
     super(props);
 
-    this.state ={
+    this.state = {
+      comics:[],
       showModal: false
     }
-
     this.onQuit = this.onQuit.bind(this);
     this.closeModal = this.closeModal.bind(this);
   }
 
+  fetchComics(){
+    const TS = new Date().getTime();
+    const HASH = CryptoJS.MD5(TS + PRIV_KEY + PUBLIC_KEY).toString();
+    axios.get( URL, {
+      params: {
+        ts: TS,
+        apikey: PUBLIC_KEY,
+        limit: 50,
+        hash: HASH
+      }
+    })
+    .then((response) => {
+      this.setComicsList(response.data.data.results);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  setComicsList(allComics){
+    let comics = allComics.filter((item) => item.thumbnail.path.split('_')[1] !== "not");
+    let comic8 = comics.slice(0, 8);//pick eight first
+    let comicGrid = [...comic8, ...comic8];// duplicate the 8 first (needed 16 in total)
+    comicGrid.sort(() =>  0.5 - Math.random());//randomize order
+    this.setState({comics: comicGrid});// set state
+  }
+
+  componentWillReceiveProps(nextProps){
+    if(nextProps.startGame){
+      this.fetchComics();
+    }
+  }
+
   closeModal(){
-    this.setState({ showModal:false });
     this.props.onStartClick();
+    this.setState({ showModal:false });
   }
 
   onQuit(){
@@ -31,7 +69,7 @@ class GamePage extends Component {
 
   render(){
     const cards = COMICS.map((item, key) => <Card key={key}/>);
-    const renderCards = this.props.startGame ? cards : <h1 className="bubble__title">Loading...</h1>;
+    const renderCards = this.state.comics.length > 0 ? cards : <h1 className="bubble__title">Loading...</h1>;
 
     const renderModal = () =>{
       if(this.state.showModal){
@@ -69,7 +107,7 @@ class GamePage extends Component {
         </div>
         <div className="home-page-footer">
           <div className="container">
-            <p className="home-page-footer--p">Rebus.io 2017 ©</p>
+            <p className="home-page-footer--p">Data provided by Marvel. © 2017 MARVEL | Rebus.io </p>
           </div>
         </div>
         {renderModal()}
